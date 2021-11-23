eef894f0cea1c99bc172f2be6ffb8a538ae1ca47
benedictfernando <fernandobenedict619@gmail.com>
2021-11-23 14:01:06 +0800

    hyperlink list items to individual person

--- src/views/ContactForm.js
+++ src/views/ContactForm.js
@@ -1,9 +1,53 @@
-const ContactForm = () => {
+
+import { useEffect, useState } from "react";
+import { Form, Button } from 'semantic-ui-react';
+import { useParams } from 'react-router';
+
+const ContactForm = (props) => {
+
+    // initialize a state variable and a setter function
+    const [person, setPerson] = useState({
+        firstname: '', lastname: '', id: null,
+        emailaddresses: [], postaladdresses:[]
+    })
+
+    // get encrypted id from url
+    const { id } = useParams();
+
+    // execute when the DOM content has been loaded
+    useEffect(() => {
+        (async () => {
+            // fetch data from jQuery api
+            const response = await fetch('/contact/' + id);
+
+            // jsonify received data, then store it into array
+            const results = await response.json();
+
+            // set new state of the 'person' state variable
+            setPerson(results);
+        })();
+    }, []);
+
+    // render the following when the this variable is called
     return (
         <>
-            <form>
-                <input type="text" name="firstname" placeholder="First name"></input>
-            </form>
+            <Form>
+                <Form.Field>
+                    <Form.Input
+                        name='firstname'
+                        label='First name'
+                        value={person.firstname}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <Form.Input
+                        name='lastname'
+                        label='Last name'
+                        value={person.lastname}
+                    />
+                </Form.Field>
+                <Button>Save</Button>
+            </Form>
         </>
     );
 }
